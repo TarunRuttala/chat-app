d5e630966bf0202c500b62642021bd04d5a4c7ab
Tarun Ruttala <tarunruttala76@gmail.com>
2024-08-22 17:39:26 +0530

    fourth commit

--- src/Components/RightSidebar/RightSideBar.jsx
+++ src/Components/RightSidebar/RightSideBar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 
 const RightSideBar = () => {
   return (
-    <div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Fcukard</h3>
+        <p>Hey, there! I am Fcukard using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic5} alt="" />
+          <img src={assets.pic6} alt="" />
+        </div>
+      </div>
       
     </div>
   )
